ceb21ecd01798b97ee248b5691f2e8d7641acc97
salomao-mafalda <salomaomachadon@gmail.com>
2022-09-30 20:33:13 -0500

    Upload code

---
+++ deeplabv3.m
@@ -0,0 +1,34 @@
+clear all; close all;
+%The width and height of the image must be a multiple of 2^EncoderDepth.
+imageSize = [224 224 3];
+numClasses = 2;
+encoderDepth = 5;
+lgraph = deeplabv3plusLayers(imageSize,numClasses,'resnet18');
+%plot(lgraph)
+
+dataSetDir = fullfile('dataset');
+imageDir = fullfile(dataSetDir,'images');
+labelDir = fullfile(dataSetDir,'labels');
+imds = imageDatastore(imageDir);
+classNames = ["tumor","background"];
+labelIDs   = [1 0];
+pxds = pixelLabelDatastore(labelDir,classNames,labelIDs);
+ds = combine(imds,pxds);
+options = trainingOptions('adam', ...
+    'MiniBatchSize',7,...
+    'InitialLearnRate',1e-3, ...
+    'MaxEpochs',10,...
+    'Plots','training-progress',...
+    'ExecutionEnvironment','cpu');
+
+net = trainNetwork(ds,lgraph,options)
+
+%imageDS.ReadFcn = @customReadDatastoreImage;
+
+I = imread("dataset\images\benign (4).png");
+C = semanticseg(I,net);
+B = labeloverlay(I,C);
+figure
+
+imshow(B)
+
